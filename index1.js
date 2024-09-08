be1ec6b5a04a7ca1cd8589cd477520392d3d9432
Кірюша <mkiril087@gmail.com>
2024-09-08 18:02:08 +0300

    пр

---
+++ index1.js
@@ -0,0 +1,60 @@
+// let age = Number(prompt(`enter your age`));
+// age += 5;
+// console.log(age);
+
+// task 2
+// const num = Number(prompt(`enter number`));
+// const half = Math.floor(num/2);
+// const maybe = half * 2;
+// let answer;
+
+// if (num === maybe && num > 10) {
+//     answer = `parne and more 10`;
+// } else {
+//     answer = `ne parne or number smaller 10`;
+// }
+
+// console.log(answer);
+
+
+// task 3
+// let askNum = Number(prompt(`enter your number`));
+// let s;
+// if (askNum >= 51) {
+//     s = `number more 51`;
+// } else {
+//     s = `number smaller or = 50`;
+// }
+
+// console.log(s);
+
+
+// task 4 
+
+// let mark = Number(prompt(`напишіть оцінку нашому сайту від 1 до 5 де 1 це жахливо а 5 неймовірно`));
+// let x;
+// if (mark === 1) {
+//     x = `ви обрали 1 а обто це означає що наш сайт жахливий, ви мене бразили, не заходьте сюди більше або я вас вичислю та перевеу усі гроші з вашої банківської картки і спалю ці гроші ;)`;
+// } else if (mark === 2) {
+//     x = `ви обрали 2 а отже це означає що наш сайт не є найгіршим але йому ще є куди рости, я образився але не так сильно тому можете заходити`;
+// } else if (mark === 3) {
+//     x = `ви обрали 3 а це означає що наш сайт є чимось середнім тому я і не образився і не в захваті`;
+// } else if (mark === 4) {
+//     x = `ви обрали 4 а це означає що наш сайт майже ідеален, дякую за відгук`;
+// } else if (mark === 5) {
+//     x = `ви обрали 5 а це означає що наш сайт ідеален що й так було ясно і без оцінки, але всеодно дякую вам за відгук;)`;
+// } else {
+//     x = `ви написали не цифру бистро беріть перезапускайте сайт і виправляйте`;
+// }
+// console.log(x);
+
+// confirm(x)
+
+
+// task 5
+
+// let username = prompt(`enter your username`)
+// let x = `вітаю ${username}`;
+// let y = `привіт ${username}`;
+// let name = username === `Адмін` ? x : y;
+// console.log(name);
